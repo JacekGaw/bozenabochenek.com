41d79dd62b552b03ce5ea12fd4f36e885a55466c
JacekGaw <jac.gawlyta@gmail.com>
2020-04-12 18:49:14 +0200

    Add Portfolio section

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const hamburgerButton = document.querySelector('#hamburgerButton');
 const hamburgerIcon = document.querySelector('#hamburgerIcon');
 const menuContainer = document.querySelector('#menu');
-
+const portfolioImages = document.querySelectorAll('.container__item');
 
 hamburgerButton.addEventListener('click', toggleMenu);
 
@@ -9,3 +9,11 @@ function toggleMenu(){
     hamburgerIcon.classList.toggle("hamburger__icon--close")
     menuContainer.classList.toggle("navigation--open");
 }
+// function getImageWidth() {
+//     for(let i=0; i<portfolioImages.length; i++){
+//         portfolioImages[i].offsetWidth = portfolioImages[i].style.width * 0.75;
+//         console.log(portfolioImages[i].style.width);
+//         portfolioImages[i].style.height = portfolioImages[i].offsetHeight;
+//     }
+// }
+// getImageWidth()
