82aa4af62df4fe2b5fb0971503cee667e7179026
JacekGaw <jac.gawlyta@gmail.com>
2020-04-20 21:24:34 +0200

    Improved photos display

--- assets/offer_photos.js
+++ assets/offer_photos.js
@@ -1,6 +1,6 @@
 const offerItems = document.querySelectorAll('.offer__item');
 
 offerItems.forEach(function(val){
-    val.style.backgroundImage = "url('/assets/img/mobile/"+val.id+".jpg')";
+    val.style.backgroundImage = "url('https://jacekgaw.github.io/bozenabochenek.com/assets/img/mobile/"+val.id+".jpg')";
     console.log(val.id);
 })
