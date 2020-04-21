9ec830347b31eec18ac4926d64f7970023fcf894
JacekGaw <jac.gawlyta@gmail.com>
2020-04-21 22:45:34 +0200

    Add another subpage and create script for gallery

---
+++ assets/gallery.js
@@ -0,0 +1,24 @@
+const imageSquares = document.querySelectorAll('.item__photo--square');
+const closeButton = document.querySelector(`#closeGallery`);
+
+// const closeGallery = () =>
+
+function closeGallery(){
+    document.querySelector(`.photo__box--open`).style.display = `none`;
+    document.querySelector(`.photo__box--open`).classList.remove(`photo__box--open`);
+    closeButton.style.display = `none`;
+};
+
+function openGallery(){
+    let nrId = this.id;
+    document.querySelector(`#${nrId}Box`).style.display = `flex`;
+    document.querySelector(`#${nrId}Box`).classList.add(`photo__box--open`);
+    closeButton.style.display = `block`;
+    closeButton.addEventListener('click',closeGallery);
+};
+
+imageSquares.forEach((element, id) => {
+    element.style.backgroundImage = `url('/assets/img/plener_slubny/plener_slubny${id}.jpg')`;
+    element.addEventListener('click',openGallery);
+});
+
