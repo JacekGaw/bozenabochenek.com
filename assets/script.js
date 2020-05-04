ba7b0865b177f0a9ce910b86050f8c0fc8431d2b
JacekGaw <jac.gawlyta@gmail.com>
2020-05-05 00:23:36 +0200

    Dropdown menu

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,19 @@ const hamburgerButton = document.querySelector('#hamburgerButton');
 const hamburgerIcon = document.querySelector('#hamburgerIcon');
 const menuContainer = document.querySelector('#menu');
 const portfolioImages = document.querySelectorAll('.container__item');
+const portfolioListItem = document.querySelector('#menuDropdown');
+const dropdownList = document.querySelector('.second__list');
 
 hamburgerButton.addEventListener('click', toggleMenu);
+portfolioListItem.addEventListener('click', toggleDropdownMenu);
 
 function toggleMenu(){
     hamburgerIcon.classList.toggle("hamburger__icon--close")
     menuContainer.classList.toggle("navigation--open");
+}
+
+function toggleDropdownMenu(){
+    dropdownList.classList.toggle("second__list--close");
 }
 
 
